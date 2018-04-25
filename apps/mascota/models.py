77b2078a9e24403d6d3279d8a855a5baa78e9095
ecreyes <eduardo.reyeso.14@sansano.usm.cl>
2018-04-24 22:20:40 -0300

    se soluciono el modelo mascota que tenia errores en los campos de field que estaba mal escrito

--- apps/mascota/models.py
+++ apps/mascota/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Mascota(models.Model):
-	nombre = models.Charfield(max_length=50)
-	sexo = models.Charfield(max_length=10)
+	nombre = models.CharField(max_length=50)
+	sexo = models.CharField(max_length=10)
 	edad_aproximada = models.IntegerField()
 	fecha_rescate = models.DateField()
 	
