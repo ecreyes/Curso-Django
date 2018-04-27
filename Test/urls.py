e34aceb5f4883a85edad0726b9fb69a6a4f8b0ca
ecreyes <eduardo.reyeso.14@sansano.usm.cl>
2018-04-26 22:02:44 -0300

    Se generaron las vistas y urls para adopcion y mascota

--- Test/urls.py
+++ Test/urls.py
@@ -18,7 +18,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('apps.mascota.urls')),
+    path('mascota/',include('apps.mascota.urls')),
     path('adopcion/',include('apps.adopcion.urls')),
 
 ]
--- apps/adopcion/urls.py
+++ apps/adopcion/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
-
+from apps.adopcion.views import index
 urlpatterns = [
+	path('',index),
 ]
