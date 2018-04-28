0f72a21c0fcfe6ebee5b603d3fd0b3ecb22a8cf6
ecreyes <eduardo.reyeso.14@sansano.usm.cl>
2018-04-27 22:02:05 -0300

    se agregaron los namespace que hacia falta

--- Test/urls.py
+++ Test/urls.py
@@ -18,7 +18,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('mascota/',include('apps.mascota.urls')),
-    path('adopcion/',include('apps.adopcion.urls')),
+    path('mascota/',include('apps.mascota.urls',namespace='mascota')),
+    path('adopcion/',include('apps.adopcion.urls',namespace='adopcion')),
 
 ]
