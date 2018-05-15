c15a4746bbab3c08e4538ea5450e694b00ca0eaa
ecreyes <eduardo.reyeso.14@sansano.usm.cl>
2018-05-15 16:25:33 -0400

    se agregó la url para editar

--- apps/mascota/urls.py
+++ apps/mascota/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from apps.mascota.views import index,mascota_view,mascota_list
+from apps.mascota.views import index,mascota_view,mascota_list,mascota_edit
 
 app_name = 'apps'
 urlpatterns = [
     path('', index,name='index'),
     path('nuevo', mascota_view,name='mascota_crear'),
     path('listar', mascota_list,name='mascota_listar'),
+    path('editar/<int:id_mascota>', mascota_edit,name='mascota_editar'),
 ]
