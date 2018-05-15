ff3ffed272fd9026f5595951538ab6d18bf6dc4a
ecreyes <eduardo.reyeso.14@sansano.usm.cl>
2018-05-15 16:50:47 -0400

    se añadio el metodo para eliminar una mascota

--- apps/mascota/views.py
+++ apps/mascota/views.py
@@ -31,3 +31,10 @@ def mascota_edit(request,id_mascota):
 			form.save()
 		return redirect('mascota:mascota_listar')
 	return render(request,'mascota/mascota_form.html',{'form':form})
+
+def mascota_delete(request,id_mascota):
+	mascota = Mascota.objects.get(id=id_mascota)
+	if request.method == 'POST':
+		mascota.delete()
+		return redirect('mascota:mascota_listar')
+	return render(request,'mascota/mascota_delete.html',{'mascota':mascota})
