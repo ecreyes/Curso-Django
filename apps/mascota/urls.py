29ae2a69e2fdc5be7c3311d9e7a44f36a92a18af
ecreyes <eduardo.reyeso.14@sansano.usm.cl>
2018-05-15 13:15:31 -0400

    se agrego la url para la ruta nuevo y se importo la vista que maneja la peticion a al ruta

--- apps/mascota/urls.py
+++ apps/mascota/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from apps.mascota.views import index
+from apps.mascota.views import index,mascota_view
 
 app_name = 'apps'
 urlpatterns = [
-    path('', index),
+    path('', index,name='index'),
+    path('nuevo', mascota_view,name='mascota_crear'),
 ]
