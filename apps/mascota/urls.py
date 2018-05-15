b974991428eab42008bdeeddf4a179f642837214
ecreyes <eduardo.reyeso.14@sansano.usm.cl>
2018-05-15 16:50:31 -0400

    se añadio la url para eliminar una mascota

--- apps/mascota/urls.py
+++ apps/mascota/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from apps.mascota.views import index,mascota_view,mascota_list,mascota_edit
+from apps.mascota.views import index,mascota_view,mascota_list,mascota_edit,mascota_delete
 
 app_name = 'apps'
 urlpatterns = [
@@ -7,4 +7,5 @@ urlpatterns = [
     path('nuevo', mascota_view,name='mascota_crear'),
     path('listar', mascota_list,name='mascota_listar'),
     path('editar/<int:id_mascota>', mascota_edit,name='mascota_editar'),
+    path('eliminar/<int:id_mascota>', mascota_delete,name='mascota_eliminar'),
 ]
