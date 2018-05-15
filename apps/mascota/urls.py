8aaa3d253f65095925c50f991ae7c66b4e929189
ecreyes <eduardo.reyeso.14@sansano.usm.cl>
2018-05-15 14:02:36 -0400

    se agrego la url de la aplicación para listar

--- apps/mascota/urls.py
+++ apps/mascota/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from apps.mascota.views import index,mascota_view
+from apps.mascota.views import index,mascota_view,mascota_list
 
 app_name = 'apps'
 urlpatterns = [
     path('', index,name='index'),
     path('nuevo', mascota_view,name='mascota_crear'),
+    path('listar', mascota_list,name='mascota_listar'),
 ]
