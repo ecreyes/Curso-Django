61dfe30d1b89533205d53985ff5b3b1e19a09af5
ecreyes <eduardo.reyeso.14@sansano.usm.cl>
2018-05-17 22:04:49 -0400

    se agregaron las rutas para los templates

--- apps/adopcion/urls.py
+++ apps/adopcion/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from apps.adopcion.views import index
+from apps.adopcion.views import index,SolicitudList,SolicitudCreate
 
 app_name = 'apps'
 urlpatterns = [
 	path('',index),
+	path('solicitud/listar',SolicitudList.as_view(),name='solicitud_listar'),
+	path('solicitud/nuevo',SolicitudCreate.as_view(),name='solicitud_crear'),
 ]
