f6778894f7426a2c365ca967dadbf5ee4828e1ab
ecreyes <eduardo.reyeso.14@sansano.usm.cl>
2018-05-17 21:16:57 -0400

    formulario para solicitud

--- apps/adopcion/forms.py
+++ apps/adopcion/forms.py
@@ -26,5 +26,21 @@ class PersonaForm(forms.ModelForm):
 			'edad':forms.TextInput(attrs={'class':'form-control'}),
 			'telefono': forms.TextInput(attrs={'class':'form-control'}),
 			'email': forms.TextInput(attrs={'class':'form-control'}),
-			'domicilio': forms.TextInput(attrs={'class':'form-control'}),
+			'domicilio': forms.Textarea(attrs={'class':'form-control'}),
+		}
+
+class SolicitudForm(forms.ModelForm):
+	class Meta:
+		model = Solicitud
+		fields = [
+			'numero_mascotas',
+			'razones',
+		]
+		labels = {
+			'numero_mascotas': 'Numero de mascotas',
+			'razones': 'Razones para adoptar',
+		}
+		widgets = {
+			'numero_mascotas': forms.TextInput(attrs={'class':'form-control'}),
+			'razones': forms.Textarea(attrs={'class':'form-control'}),
 		}
