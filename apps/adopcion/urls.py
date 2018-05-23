21f778a8169893c0febf8c12fbb97c277935a9ad
ecreyes <eduardo.reyeso.14@sansano.usm.cl>
2018-05-22 23:05:21 -0400

    se implemento la ruta para editar

--- apps/adopcion/urls.py
+++ apps/adopcion/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from apps.adopcion.views import index,SolicitudList,SolicitudCreate
+from apps.adopcion.views import index,SolicitudList,SolicitudCreate,SolicitudUpdate
 
 app_name = 'apps'
 urlpatterns = [
 	path('',index),
 	path('solicitud/listar',SolicitudList.as_view(),name='solicitud_listar'),
 	path('solicitud/nuevo',SolicitudCreate.as_view(),name='solicitud_crear'),
+	path('solicitud/editar/<int:pk>',SolicitudUpdate.as_view(),name='solicitud_editar'),
 ]
