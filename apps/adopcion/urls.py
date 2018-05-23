d78972ab1d4014da9fb9c761f2d83e5bcda67bf5
ecreyes <eduardo.reyeso.14@sansano.usm.cl>
2018-05-22 23:11:58 -0400

    se agrego la ruta de eliminar

--- apps/adopcion/urls.py
+++ apps/adopcion/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from apps.adopcion.views import index,SolicitudList,SolicitudCreate,SolicitudUpdate
+from apps.adopcion.views import index,SolicitudList,SolicitudCreate,SolicitudUpdate,SolicitudDelete
 
 app_name = 'apps'
 urlpatterns = [
@@ -7,4 +7,5 @@ urlpatterns = [
 	path('solicitud/listar',SolicitudList.as_view(),name='solicitud_listar'),
 	path('solicitud/nuevo',SolicitudCreate.as_view(),name='solicitud_crear'),
 	path('solicitud/editar/<int:pk>',SolicitudUpdate.as_view(),name='solicitud_editar'),
+	path('solicitud/eliminar/<int:pk>',SolicitudDelete.as_view(),name='solicitud_eliminar'),
 ]
